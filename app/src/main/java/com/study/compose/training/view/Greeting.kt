a5fa30b677ae1839428ff5c76107032627bf4584
NeeoMacLynch <422910212@qq.com>
2021-07-02 13:50:44 +0800

    code opt: 优化布局

--- app/src/main/java/com/study/compose/training/view/Greeting.kt
+++ app/src/main/java/com/study/compose/training/view/Greeting.kt
@@ -1,16 +1,30 @@
 package com.study.compose.training.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
+/**
+ * lynch
+ * */
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name")
+    MaterialTheme {
+        Text(
+            text = "您好 $name",
+            style = typography.body2,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+    }
 }
 
 @Preview
 @Composable
 fun PreviewGreeting(){
-    Greeting(name = "preview")
+    Greeting(name = "登录者")
 }
